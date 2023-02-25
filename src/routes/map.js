5561b46e019a137911ccca24a2cf0d8e10a5f2d9
FOBshippingpoint <aulaulql4ql4xj6@gmail.com>
2023-02-26 01:05:01 +0800

    feat: add fly to user location

--- src/routes/map.js
+++ src/routes/map.js
@@ -1,105 +1,135 @@
-import { PUBLIC_UPDATE_DATE } from '$env/static/public';
-import { getCountyData } from '$lib/utils/county-data';
-import { parseData } from '$lib/utils/csv';
-import Popup from './Popup.svelte';
+import { PUBLIC_UPDATE_DATE } from "$env/static/public";
+import { getCountyData } from "$lib/utils/county-data";
+import { parseData } from "$lib/utils/csv";
+import Popup from "$lib/components/Popup.svelte";
+
+function onGetUserLatLng(cb) {
+  if (!navigator.geolocation) {
+    console.log("Geolocation is not supported by this browser.");
+    return;
+  }
+  navigator.geolocation.getCurrentPosition((position) => {
+    const { latitude, longitude } = position.coords;
+    cb([latitude, longitude]);
+  }, (error) => {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        console.log("User denied the request for Geolocation.");
+        break;
+      case error.POSITION_UNAVAILABLE:
+        console.log("Location information is unavailable.");
+        break;
+      case error.TIMEOUT:
+        console.log("The request to get user location timed out.");
+        break;
+    }
+  });
+}
 
 export async function initMap() {
-	const L = await import('leaflet');
-	const map = L.map('map', { preferCanvas: true }).setView([25.0596, 121.4951], 13);
+  const L = await import("leaflet");
+  const map = L.map("map", { preferCanvas: true }).setView(
+    [25.0453, 121.5403],
+    13,
+  );
 
-	// base layer
-	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 19,
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(map);
+  onGetUserLatLng((latlng) => {
+    map.flyTo(latlng);
+  });
 
-	// WMTS 村里界圖層
-	const village = L.tileLayer(
-		'https://wmts.nlsc.gov.tw/wmts/Village/default/GoogleMapsCompatible/{z}/{y}/{x}',
-		{
-			maxZoom: 19
-		}
-	).addTo(map);
+  // base layer
+  const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
 
-	map.attributionControl.addAttribution(
-		'村里界圖 &copy; <a href="https://data.gov.tw/dataset/17219">內政部國土測繪中心</a>'
-	);
-	map.attributionControl.addAttribution(
-		'人行道標註資料 &copy; <a href="https://commutag.agawork.tw/dataset?id=63528cc34f042e88cc951433">平安走路許願帳戶-行人庇護空間</a>'
-	);
-	map.attributionControl.addAttribution('資料更新時間：' + PUBLIC_UPDATE_DATE);
+  // WMTS 村里界圖層
+  const village = L.tileLayer(
+    "https://wmts.nlsc.gov.tw/wmts/Village/default/GoogleMapsCompatible/{z}/{y}/{x}",
+    {
+      maxZoom: 19,
+    },
+  ).addTo(map);
 
-	function rank2Color(rank) {
-		return rank > 8
-			? '#33FF33'
-			: rank > 6
-			? '#99FF66'
-			: rank > 4
-			? '#FFFF66'
-			: rank > 2
-			? '#FF9933'
-			: '#FF0000';
-	}
+  map.attributionControl.addAttribution(
+    '村里界圖 &copy; <a href="https://data.gov.tw/dataset/17219">內政部國土測繪中心</a>',
+  );
+  map.attributionControl.addAttribution(
+    '人行道標註資料 &copy; <a href="https://commutag.agawork.tw/dataset?id=63528cc34f042e88cc951433">平安走路許願帳戶-行人庇護空間</a>',
+  );
+  map.attributionControl.addAttribution("資料更新時間：" + PUBLIC_UPDATE_DATE);
 
-	const legend = L.control({ position: 'bottomright' });
+  function rank2Color(rank) {
+    return rank > 8
+      ? "#33FF33"
+      : rank > 6
+      ? "#99FF66"
+      : rank > 4
+      ? "#FFFF66"
+      : rank > 2
+      ? "#FF9933"
+      : "#FF0000";
+  }
 
-	legend.onAdd = function (map) {
-		const div = L.DomUtil.create('div', 'legend'),
-			grades = [0, 2, 4, 6, 8, 10],
-			labels = [];
+  const legend = L.control({ position: "bottomright" });
 
-		div.innerHTML += '<div>a1分數</div>';
-		// loop through our density intervals and generate a label with a colored square for each interval
-		for (let i = 0; i < grades.length - 1; i++) {
-			div.innerHTML +=
-				'<i style="background:' +
-				rank2Color(grades[i] + 1) +
-				'"></i> ' +
-				grades[i] +
-				(grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-		}
-		return div;
-	};
+  legend.onAdd = function (map) {
+    const div = L.DomUtil.create("div", "legend"),
+      grades = [0, 2, 4, 6, 8, 10],
+      labels = [];
 
-	legend.addTo(map);
+    div.innerHTML += "<div>a1分數</div>";
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (let i = 0; i < grades.length - 1; i++) {
+      div.innerHTML += '<i style="background:' +
+        rank2Color(grades[i] + 1) +
+        '"></i> ' +
+        grades[i] +
+        (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
 
-	const data = await parseData();
-	const countyData = await getCountyData();
-	const allMarker = L.layerGroup();
-	data.forEach(addRow);
-	async function addRow(row) {
-		const marker = L.circleMarker([row.lat, row.lng], {
-			color: rank2Color(row.rankA1),
-			fillOpacity: 0.5
-		}).addTo(allMarker);
+  legend.addTo(map);
 
-		// add all markers to corresponding layer (county, town, village)
-		countyData.map((c) => {
-			if (row.countyName == c.name) {
-				c.layer.addLayer(marker);
-			}
-			c.towns.map((t) => {
-				if (row.townName == t.name) {
-					t.layer.addLayer(marker);
-				}
-				t.villages.map((v) => {
-					if (row.villName == v.name) {
-						v.layer.addLayer(marker);
-					}
-				});
-			});
-		});
-		marker.bindPopup(() => {
-			const container = L.DomUtil.create('div');
-			new Popup({
-				target: container,
-				props: { row }
-			});
-			return container;
-		});
-	}
+  const data = await parseData();
+  const countyData = await getCountyData();
+  const allMarker = L.layerGroup();
+  data.forEach(addRow);
+  async function addRow(row) {
+    const marker = L.circleMarker([row.lat, row.lng], {
+      color: rank2Color(row.rankA1),
+      fillOpacity: 0.5,
+    }).addTo(allMarker);
 
-	map.addLayer(allMarker);
+    // add all markers to corresponding layer (county, town, village)
+    countyData.map((c) => {
+      if (row.countyName == c.name) {
+        c.layer.addLayer(marker);
+      }
+      c.towns.map((t) => {
+        if (row.townName == t.name) {
+          t.layer.addLayer(marker);
+        }
+        t.villages.map((v) => {
+          if (row.villName == v.name) {
+            v.layer.addLayer(marker);
+          }
+        });
+      });
+    });
+    marker.bindPopup(() => {
+      const container = L.DomUtil.create("div");
+      new Popup({
+        target: container,
+        props: { row },
+      });
+      return container;
+    });
+  }
 
-	return { map, layers: { village, allMarker } };
+  map.addLayer(allMarker);
+
+  return { map, layers: { village, allMarker } };
 }
